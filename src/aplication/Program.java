08e714a02fe639a06722a099a9d4a40d3139f5c0
Vitor Almeida <pro.vitoralmeida@gmail.com>
2019-05-21 20:48:53 -0300

    update implementation

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -36,5 +36,11 @@ public class Program {
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
 
+        System.out.println("\n=== TEST 4: seller insert ===");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
+
     }
 }
